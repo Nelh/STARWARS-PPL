012edf814472e8c52cd20477c5377e1a9e2f1bc5
Nelh <amstrongnouni62@gmail.com>
2021-03-25 21:33:28 +0200

    add Client files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import './App.css';
+import './App.scss';
 
 function App() {
   const client = new ApolloClient({
@@ -9,7 +9,15 @@ function App() {
 
   return (
     <ApolloProvider client={client}>
-      <div></div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand">Navbar</a>
+          <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
     </ApolloProvider>
   );
 }
