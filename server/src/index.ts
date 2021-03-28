45de46504c722f64fd7e3d0bae5bd3095ade6642
Nelh <amstrongnouni62@gmail.com>
2021-03-28 21:05:29 +0200

    Material change

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,9 +14,9 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 // production ready
-app.use(express.static('public'));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
 let PORT = process.env.PORT || 4000;
